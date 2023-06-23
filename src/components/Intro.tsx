b8b6de66d68032cb457d61f561de1ba118317091
Gabriel Oliveira <gabriellfernandoo@gmail.com>
2023-06-23 13:04:00 -0300

    feat: final tweaks to the intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,4 +1,7 @@
 import * as styled from '@/styles/components/Intro'
+import Image from 'next/image'
+
+import AssetTitle from '@/assets/asset-header.png'
 
 export default function Intro() {
   return (
@@ -6,7 +9,13 @@ export default function Intro() {
       <styled.Container>
         <p className="tag">Webinars Exclusivos</p>
         <h2>Menos Conversinha,</h2>
-        <h1>Mais Conversão</h1>
+        <div className="title">
+          <h1>Mais Conversão</h1>
+          <Image
+            src={AssetTitle}
+            alt="icone para estilizar o titulo principal"
+          />
+        </div>
         <p>
           Conheça as estratégias que <strong>mudaram o jogo</strong> e como
           aplicá-las no seu negócio
--- src/styles/components/Intro.ts
+++ src/styles/components/Intro.ts
@@ -1,7 +1,7 @@
 import { styled } from 'styled-components'
 
 export const Intro = styled.section`
-  padding: 8rem 0;
+  padding: 10rem 0;
   width: 100vw;
   background-color: #f0f8ff;
 `
@@ -21,23 +21,32 @@ export const Container = styled.div`
     border: 1px solid #007eff;
   }
 
-  h1 {
-    font-size: 5.375rem;
-    font-weight: 800;
-    line-height: 1;
-    color: #1f76f0;
+  .title {
+    display: flex;
 
-    &::after {
-      content: '';
-      display: block;
-      height: 1px;
-      background-color: #d2e2e2;
-      margin: 1.5rem 0;
+    h1 {
+      font-size: 5.375rem;
+      font-weight: 800;
+      line-height: 1;
+      color: #1f76f0;
+      margin-right: -2rem;
+
+      &::after {
+        content: '';
+        display: block;
+        height: 1px;
+        background-color: #d2e2e2;
+        margin: 1.5rem 0;
+      }
     }
   }
 
   h2 {
     font-size: 3rem;
     font-weight: 100;
+  }
+
+  p {
+    font-size: 1.125rem;
   }
 `
