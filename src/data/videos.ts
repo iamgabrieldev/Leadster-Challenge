db9f15914148b739b8b80422ae3c11b0192842fc
Gabriel Oliveira <gabriellfernandoo@gmail.com>
2023-06-27 16:04:33 -0300

    feat: add mock videos in format json

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,5 +1,65 @@
 export const videos = [
   {
+    id: 1,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
     url: 'https://www.youtube.com/embed/eVwTlOuzT0Q',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
+  },
+  {
+    id: 2,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
+    url: 'https://www.youtube.com/embed/85k8se4Zo70',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
+  },
+  {
+    id: 3,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
+    url: 'https://www.youtube.com/embed/Zu-D9A_L5Qo',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
+  },
+  {
+    id: 4,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
+    url: 'https://www.youtube.com/embed/eVwTlOuzT0Q',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
+  },
+  {
+    id: 5,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
+    url: 'https://www.youtube.com/embed/85k8se4Zo70',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
+  },
+  {
+    id: 6,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
+    url: 'https://www.youtube.com/embed/Zu-D9A_L5Qo',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
+  },
+  {
+    id: 7,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
+    url: 'https://www.youtube.com/embed/eVwTlOuzT0Q',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
+  },
+  {
+    id: 8,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
+    url: 'https://www.youtube.com/embed/85k8se4Zo70',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
+  },
+  {
+    id: 9,
+    title: 'Como aumentar sua Geração de Leads feat. Traktor',
+    url: 'https://www.youtube.com/embed/Zu-D9A_L5Qo',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore iusto distinctio porro molestias ducimus reprehenderit unde modi dolor non animi accusamus alias quisquam cumque, rem tempora magni esse quos! Autem?',
   },
 ]
